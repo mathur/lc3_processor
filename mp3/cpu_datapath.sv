661a5db9e29a692d63b333507f560c41fbc8f9a8
Rohan Mathur <rohanmathur34@gmail.com>
2017-03-10 13:05:56 -0600

    push initial cpu datapath
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- mp3/cpu_datapath.sv
+++ mp3/cpu_datapath.sv
@@ -5,4 +5,49 @@ module cpu_datapath
     input clk
 );
 
+if_datapath if
+(
+    .clk(clk)
+);
+
+buffer if_id_buf
+(
+    .clk(clk)
+);
+
+id_datapath id
+(
+    .clk(clk)
+);
+
+buffer id_ex_buf
+(
+    .clk(clk)
+);
+
+ex_datapath ex
+(
+    .clk(clk)
+);
+
+buffer ex_mem_buf
+(
+    .clk(clk)
+);
+
+mem_datapath mem
+(
+    .clk(clk)
+);
+
+buffer mem_wb_buf
+(
+    .clk(clk)
+);
+
+wb_datapath wb
+(
+    .clk(clk)
+);
+
 endmodule : cpu_datapath
