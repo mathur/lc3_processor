e58ee15024af4bb3a4ed125639f4ee51c3944688
Rohan Mathur <rohanmathur34@gmail.com>
2017-03-10 12:38:27 -0600

    Wipe out previous datapath/control logic
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- mp3/if_datapath.sv
+++ mp3/if_datapath.sv
@@ -8,34 +8,10 @@ ir ir_unit (
     .clk(clk),
     .load(load_ir),
     .in(mdr_out),
-    .opcode(opcode),
+    .instruction(instruction),
     .dest(dest),
     .src1(sr1),
-    .src2(sr2),
-    .offset6(offset6),
-    .offset9(offset9),
-    .offset5(offset5),
-    .offset11(offset11),
-    .jsr_mode(jsr_mode),
-    .imm_mode(imm_mode),
-    .trap_vector(trap_vector),
-    .imm4(ir_imm4),
-    .a(shift_a),
-    .d(shift_d)
-);
-
-plus2 plus_2
-(
-    .in(pc_out),
-    .out(pc_plus2_out)
-);
-
-register pc
-(
-    .clk(clk),
-    .load(load_pc),
-    .in(pcmux_out),
-    .out(pc_out)
+    .src2(sr2)
 );
 
 mux4 pcmux
@@ -46,6 +22,20 @@ mux4 pcmux
     .c(sr1_out),
     .d(mdr_out),
     .f(pcmux_out)
+);
+
+register pc
+(
+    .clk(clk),
+    .load(load_pc),
+    .in(pcmux_out),
+    .out(pc_out)
+);
+
+plus2 plus_2
+(
+    .in(pc_out),
+    .out(pc_plus2_out)
 );
 
 endmodule : if_datapath
