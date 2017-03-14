2b1485664340c2efb0435032e5a8d78489303507
Rohan Mathur <rohanmathur34@gmail.com>
2017-03-14 18:46:33 -0500

    oops
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- mp3/mem_datapath.sv
+++ mp3/mem_datapath.sv
@@ -18,11 +18,9 @@ module mem_datapath (
     output logic br_en
 );
 
-lc3b_word trap_zext_out, marmux_out, mdrmux_out, ldbmux_out, zext_8_out, shift_out;
+lc3b_word trap_zext_out, marmux_out, mdrmux_out, ldbmux_out, zext_8_out, shift_out, ldb_zext_out;
 lc3b_nzp gencc_out, cc_out;
 logic shift_a, shift_d;
-
-ldb_zext_out
 
 always_comb
 begin
