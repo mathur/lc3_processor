78674b38cceb24555da5c9367ada190f85c66f6b
Rohan Mathur <rohanmathur34@gmail.com>
2017-04-05 11:18:38 -0500

    add support for unconditional branches
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- mp3/benable.sv
+++ mp3/benable.sv
@@ -1,13 +1,18 @@
 module benable (
-input n, z, p,
-input nr, zr, pr,
-output logic enable
+    input n, z, p,
+    input nr, zr, pr,
+    output logic enable
 );
+
 always_comb
 begin
-if((n == 1 && nr == 1) || (z == 1 && zr == 1) || (p == 1 && pr == 1))
-enable = 1;
-else
-enable = 0;
+    if(nr == 1'b1 && zr == 1'b1 && pr == 1'b1) begin
+        enable = 1;
+    end else if((n == 1 && nr == 1) || (z == 1 && zr == 1) || (p == 1 && pr == 1)) begin
+        enable = 1;
+    end else begin
+        enable = 0;
+    end
 end
+
 endmodule : benable
